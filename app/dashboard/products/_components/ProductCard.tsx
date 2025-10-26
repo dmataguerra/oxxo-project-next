83f624f0374aef9a463bd77ab2ace04b0ab4b282
DavidM <dmataguerra@outlook.com>
2025-10-25 19:56:21 -0600

    feat : structure products page

---
+++ app/dashboard/products/_components/ProductCard.tsx
@@ -0,0 +1,18 @@
+import {Card, CardHeader, CardBody, Divider} from "@nextui-org/react"
+import {Product} from "@/entities"
+
+
+export default function ProductCard ({product} : {product : Product}) {
+    return (
+        <Card className="w-full max-w-xs shadow-md hover:shadow-lg hover:scale-105 transition-all">
+            <CardHeader className="px-4 py-3">
+                <div className="text-sm font-medium">{product.productName}</div>
+            </CardHeader>
+            <Divider />
+            <CardBody className="px-4 py-3 text-sm">
+                <p className="text-xs">Nombre del producto: <span className="font-bold">{product.productName}</span></p>
+                <p className="mt-2 text-xs">Precio del producto: <span className="font-bold">{product.price}</span></p>
+            </CardBody>
+        </Card>
+    )
+}
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,27 @@
-const ProductsPage = () => {
-    return null;
+import { authHeaders } from "@/helpers/authHeaders";
+import { API_URL } from "@/constants";
+import { Product } from "@/entities";
+import ProductCard from "./_components/ProductCard";
+
+const ProductsPage = async () => {
+    const response  = fetch(`${API_URL}/products`, {
+        headers: {
+            ...authHeaders(),
+        },
+        next: {
+            tags: ["dashboard:products"]
+        }
+    });
+    const products : Product[] = await (await response).json();
+    return (
+        <div>
+            {products.map((product) => {
+                return (
+                    <ProductCard key={product.productId} product={product} />
+                )
+            })}
+        </div>
+    )
 }
 
 export default ProductsPage;
