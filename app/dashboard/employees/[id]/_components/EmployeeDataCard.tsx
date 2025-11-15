1b3f5e982f13e816db0a68854cbb22d6b886b0e6
DavidM <dmataguerra@outlook.com>
2025-11-15 15:22:31 -0600

    feat : add employee card

---
+++ app/dashboard/employees/[id]/_components/EmployeeDataCard.tsx
@@ -0,0 +1,24 @@
+import { Employee } from "@/entities";
+import { Image } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function EmployeeDataCard({ employee }: { employee: Employee }) {
+    return (
+        <div className="flex flex-row items-center gap-2 bg-white rounded-md flex-grow-0 h-fit px-4  m-2 py-2 border-orange-300">
+            <div className="text-xl ">
+                <h1 className="font-bold">{employee.employeeName + " " + employee.employeeLastName}</h1>
+                <h1>{employee.employeeEmail}</h1>
+                <h1>{employee.employeePhoneNumber}</h1>
+                <Link className="underline" href={{ pathname: `/dashboard`, query: {store : String(employee.location?.locationId)  }}}>
+                    {employee.location?.locationName}
+                </Link>
+            </div>
+            <div className="h-full py-20 w-1 bg-zinc-400 mx-5" />
+            <Image src={employee.employeePhoto}
+                isZoomed
+                className="object-cover"
+                classNames={{ img: "size-60" }}
+            />
+        </div>
+    )
+}
--- app/dashboard/employees/[id]/page.tsx
+++ app/dashboard/employees/[id]/page.tsx
@@ -1,28 +1,20 @@
 import { API_URL } from "@/constants";
 import { authHeaders } from "@/helpers/authHeaders";
 import { Employee } from "@/entities";
-import EmployeeCard from "../_components/EmployeeCard";
-import { Image } from "@nextui-org/react";
 import FormUpdateEmployee from "./_components/FormUpdateEmployee";
+import EmployeeDataCard from "./_components/EmployeeDataCard";
 
-export default async function EmployeePage({params} : {params: {id: string}}) {
+export default async function EmployeePage({ params }: { params: { id: string } }) {
     const response = await fetch(`${API_URL}/employees/${params.id}`, {
         headers: {
             ...authHeaders(),
         },
     });
     const employee: Employee = await response.json();
-    return(
-        <div className = "w-full h-[90vh] flex flex-row">
-            <div>
-                <EmployeeCard employee={employee}/>
-                <Image src = {employee.employeePhoto}
-                isZoomed
-                className="object-cover"
-                classNames={{img : "size-60"}}
-                />
-            </div>
-            <FormUpdateEmployee employee={employee}/>
+    return (
+        <div className="w-full h-[90vh] flex flex-row">
+            <EmployeeDataCard employee={employee} /> 
+            <FormUpdateEmployee employee={employee} />
         </div>
-    ) ;
+    );
 }
